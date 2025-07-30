aeaa8f1f92d72d3f09314a64a3863e5071e1811c
Abel Umoh <umohsenene@gmail.com>
2025-07-31 00:48:39 +0100

    update

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ import { albertSans, montserratAlt } from "@/lib/fonts";
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-10 sm:space-y-0 sm:flex-row py-5 px-10">
-      <div className={`flex-1 flex flex-col justify-center sm:px-20 gap-y-3`}>
+    <div className="flex flex-col space-y-10 md:space-y-0 md:flex-row py-5 px-10">
+      <div className={`flex-1 flex flex-col justify-center md:px-20 gap-y-3`}>
         <p
           className={`${albertSans.className} text-[16px] font-[500] uppercase text-[#1E1E1E]`}
         >
